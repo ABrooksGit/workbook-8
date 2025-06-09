aec47d52c353da1a22bea091555cbd59a018f133
ABrooksGit <Abrooks6211996@gmail.com>
2025-06-09 10:58:17 -0400

    demo

---
+++ Demo1/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+
+    }
+}
